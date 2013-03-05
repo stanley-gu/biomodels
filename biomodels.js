3741e61631fefbd88cf819f2de698de0e80dd844
Stanley Gu <stanleygu@gmail.com>
2013-03-05 15:12:27 -0800

    modified path to work in node_modules

--- biomodels.js
+++ biomodels.js
@@ -1,11 +1,8 @@
 // Call biomodels api
 
 var java = require("java");
-java.classpath.push("./jars/biomodels-wslib_standalone-1.21.jar");
-java.classpath.push("./jars/MiriamJavaLib_standalone-1.1.5.jar");
+java.classpath.push("node_modules/biomodels/jars/biomodels-wslib_standalone-1.21.jar");
+java.classpath.push("node_modules/biomodels/jars/MiriamJavaLib_standalone-1.1.5.jar");
 
 exports.BioModelsWSClient = java.newInstanceSync("uk.ac.ebi.biomodels.ws.BioModelsWSClient");
 exports.MiriamLink = java.newInstanceSync('uk.ac.ebi.miriam.lib.MiriamLink');
-
-//client.getModelNameByIdSync('MODEL7893871775');
-//client.getAllModelsIdSync();
