06b08ae778647ec7c3ba5c6169adf8ef0dcf5c9c
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-01-25 21:35:51 +0100

    fix: Invalid path to jars files

--- biomodels.js
+++ biomodels.js
@@ -1,8 +1,9 @@
 // Call biomodels api
+var join = require('path').join;
 
 var java = require("java");
-java.classpath.push("node_modules/biomodels/jars/biomodels-wslib_standalone-1.21.jar");
-java.classpath.push("node_modules/biomodels/jars/MiriamJavaLib_standalone-1.1.5.jar");
+java.classpath.push(join(__dirname,"jars/biomodels-wslib_standalone-1.21.jar"));
+java.classpath.push(join(__dirname,"jars/MiriamJavaLib_standalone-1.1.5.jar"));
 
 exports.BioModelsWSClient = java.newInstanceSync("uk.ac.ebi.biomodels.ws.BioModelsWSClient");
 exports.MiriamLink = java.newInstanceSync('uk.ac.ebi.miriam.lib.MiriamLink');
